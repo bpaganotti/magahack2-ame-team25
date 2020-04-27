0c816cfcae651b2353c85fb0ba857ea53b828f91
bpaganotti <bpaganotti@gmail.com>
2020-04-27 19:14:33 -0300

    add angular project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { OptionAComponent } from "./pages/option-a/option-a.component";
+import { OptionBComponent } from "./pages/option-b/option-b.component";
+import { OptionCComponent } from "./pages/option-c/option-c.component";
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  {
+    path: "option-a",
+    component: OptionAComponent,
+    data: { state: "option-a" },
+  },
+  {
+    path: "option-b",
+    component: OptionBComponent,
+    data: { state: "option-b" },
+  },
+  {
+    path: "option-c",
+    component: OptionCComponent,
+    data: { state: "option-c" },
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+    data: { state: "home" },
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
